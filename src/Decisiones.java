a144946cf7f4aa72e9b3453075f47ae6525ba172
afr1993 <afr.1993@gmail.com>
2024-09-25 11:33:16 -0700

    alura java primer curso

---
+++ src/Decisiones.java
@@ -0,0 +1,15 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+
+        int fechaDeLanzamiento = 2023;
+        boolean incluidoEnPlan = true;
+        double calificacion=9.8;
+
+        if ( fechaDeLanzamiento >= 2022){
+            System.out.println("Peliculas mas populares");
+        }else{
+            System.out.println("Peliculas Retro que aun vale la pena ver");
+        }
+    }
+}
